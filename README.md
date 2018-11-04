90a8621b5e89094b1e7ce21eafaba8c6a3b1d7b2
Nabendu <nabendu.biswas@tesco.com>
2018-11-04 23:24:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+##Steps of running this project
+
+from the command prompt clone the project
+
+* $git clone https://github.com/techsithgit/react-higher-order-components.git
+* $cd react-higher-order-components
+* $npm install
+* $npm start
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import './components/ButtonOne';
+import ButtonOne from './components/ButtonOne';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ButtonOne />
+        <ButtonOne disable/>
+      </div>
+    );
+  }
+}
+
+export default App;
